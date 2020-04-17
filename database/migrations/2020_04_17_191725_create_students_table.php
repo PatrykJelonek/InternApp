8d2ee2d4c8acaf6e11864aefc0ce061668c30589
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 21:31:26 +0200

    Migration to create `students` table

---
+++ database/migrations/2020_04_17_191725_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('student_index',8);
+            $table->integer('semester');
+            $table->integer('study_year');
+            $table->foreign('specialization_id')->references('id')->on('specializations');
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('students');
+    }
+}
