e5152dcd3f5f3b358334af5baa5611b78a1631f1
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 21:53:24 +0200

    Migration to create `permissions` table

---
+++ database/migrations/2020_04_17_194909_create_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',16)->unique();
+            $table->string('display_name',32)->nullable();
+            $table->string('description', 128)->nullable();
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('permissions');
+    }
+}
