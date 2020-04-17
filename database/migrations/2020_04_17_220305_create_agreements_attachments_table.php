6b228f74158224b6b7a528e25db8bc1e0409bf74
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 00:05:58 +0200

    Migration to create `agreements_attachments` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

---
+++ database/migrations/2020_04_17_220305_create_agreements_attachments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgreementsAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('agreements_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('agreement_id')->references('id')->on('agreements');
+            $table->foreign('attachment_id')->references('id')->on('attachments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('agreements_attachments');
+    }
+}
