d241760bd2b3c907e3b02accdb496ebf4e2da9e7
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 22:13:58 +0200

    Migration to create `permissions_roles` table

---
+++ database/migrations/2020_04_17_200832_create_permissions_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions_roles', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('permission_id')->references('id')->on('permissions');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('permissions_roles');
+    }
+}
