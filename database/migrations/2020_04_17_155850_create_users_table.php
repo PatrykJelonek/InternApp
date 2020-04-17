fbeec7a5fdfcb6ec9521fb640d88c41d48f67274
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 18:20:31 +0200

    Migration to create `Users` and `User_Statuses` table

---
+++ database/migrations/2020_04_17_155850_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 64)->unique();
+            $table->string('password_hash', 255);
+            $table->string('password_reset_token', 255);
+            $table->string('first_name', 64);
+            $table->string('last_name', 64);
+            $table->string('phone_number', 16);
+            $table->rememberToken();
+            $table->foreign('user_status_id')->references('id')->on('user_statuses');
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2020_04_17_160943_create_user_statuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 16)->unique();
+            $table->string('description', 128)->nullable($value=true);
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_statuses', function (Blueprint $table) {
+            //
+        });
+    }
+}
