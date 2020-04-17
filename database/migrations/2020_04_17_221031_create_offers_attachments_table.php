4fc5cd257857db65ee5a46b43317f41676fbe204
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 00:12:59 +0200

    Migration to create `offers_attachments` table

---
+++ database/migrations/2020_04_17_221031_create_offers_attachments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('offers_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('offer_id')->references('id')->on('offers');
+            $table->foreign('attachment_id')->references('id')->on('attachments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('offers_attachments');
+    }
+}
