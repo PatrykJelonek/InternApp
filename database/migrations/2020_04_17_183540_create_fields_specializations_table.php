bcba798a4a4830479f78def690f09c3bd01d2882
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 20:39:24 +0200

    Migration to create `fields_specializations` table

---
+++ database/migrations/2020_04_17_183540_create_fields_specializations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldsSpecializationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fields_specializations', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('field_id')->references('id')->on('fields');
+            $table->foreign('specialization_id')->references('id')->on('specializations');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fields_specializations');
+    }
+}
