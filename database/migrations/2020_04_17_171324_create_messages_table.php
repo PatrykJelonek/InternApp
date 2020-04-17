864ed57e67641089889f208abcb7dad909879187
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 19:19:00 +0200

    Migration to create `Messages` table

---
+++ database/migrations/2020_04_17_171324_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('content', 255);
+            $table->foreign('from_user_id')->references('id')->on('users');
+            $table->foreign('to_user_id')->references('id')->on('users');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            //
+        });
+    }
+}
