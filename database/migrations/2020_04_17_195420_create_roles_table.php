90e6730f430c0a4e7706337406a72137877180c5
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 21:56:38 +0200

    Migration to create `roles` table

---
+++ database/migrations/2020_04_17_195420_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',16)->unique();
+            $table->string('display_name',32)->nullable();
+            $table->string('description', 128)->nullable();
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('roles');
+    }
+}
