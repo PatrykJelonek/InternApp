88ec3a942f50a816a6030007da152437cc7175fc
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 22:01:16 +0200

    Migration to create `users_roles` table

---
+++ database/migrations/2020_04_17_195752_create_users_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users_roles', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users_roles');
+    }
+}
