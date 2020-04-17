3bb974185e80c29646c31231ac78c454c885198a
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 20:21:31 +0200

    Migration to create `specializations` table

---
+++ database/migrations/2020_04_17_181736_create_specializations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecializationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('specializations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64)->unique();
+            $table->string('description', 128)->nullable();
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('specializations');
+    }
+}
