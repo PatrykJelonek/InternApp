612a7cd4de21e5880b1ed048e2c0a971f423a520
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 22:53:50 +0200

    Migration to create `questionnaire_question_answers` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

---
+++ database/migrations/2020_04_17_204359_create_questionnaire_question_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionnaireQuestionAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questionnaire_question_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('questionnaire_question_id')->references('id')->on('questionnaire_questions');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('content', 255);
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questionnaire_question_answers');
+    }
+}
