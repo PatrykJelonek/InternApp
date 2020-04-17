29ac664e97fc886804f5041dce3e2265ad934cb1
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 22:24:44 +0200

    Migration to create `questionnaires` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

---
+++ database/migrations/2020_04_17_201721_create_questionnaires_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionnairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questionnaires', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64)->unique();
+            $table->longText('description');
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questionnaires');
+    }
+}
