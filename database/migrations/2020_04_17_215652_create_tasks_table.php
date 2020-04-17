f3adeca674705de15b8f97b05b8f0a39b16daf17
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 00:01:34 +0200

    Migration to create `tasks` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

---
+++ database/migrations/2020_04_17_215652_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->longText('description');
+            $table->foreign('internship_id')->references('id')->on('internships');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('done')->default(false);
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
