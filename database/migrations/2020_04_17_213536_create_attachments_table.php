e93636a69c8ae39cbdaaa6b044cb6be15b5e8aff
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-17 23:43:37 +0200

    Migration to create `attachments` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

---
+++ database/migrations/2020_04_17_213536_create_attachments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',128)->unique();
+            $table->string('description', 255)->nullable();
+            $table->string('type', 8);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('attachments');
+    }
+}
