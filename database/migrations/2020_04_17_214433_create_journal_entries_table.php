2f09e09065dcccc92a795e7762dbb38860e3a655
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-17 23:55:07 +0200

    Migration to create `journal_entries` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

---
+++ database/migrations/2020_04_17_214433_create_journal_entries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJournalEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('journal_entries', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('internship_id')->references('id')->on('internships');
+            $table->longText('content');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('accepted')->default(false);
+            $table->dateTime('created_at', 0);
+            $table->dateTime('updated_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('journal_entries');
+    }
+}
