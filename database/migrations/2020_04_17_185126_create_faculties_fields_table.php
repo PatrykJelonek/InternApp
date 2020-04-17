816da9b95db240b0ffc4c367b5b0fc1de5a1e031
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-04-17 20:53:20 +0200

    Migration to create `faculties_fields` table

---
+++ database/migrations/2020_04_17_185126_create_faculties_fields_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacultiesFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('faculties_fields', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('faculty_id')->references('id')->on('faculties');
+            $table->foreign('field_id')->references('id')->on('fields');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('faculties_fields');
+    }
+}
