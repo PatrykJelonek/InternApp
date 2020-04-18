334944a3d8a620108d5f222e24aacbfb1fc2fea4
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 16:33:41 +0200

    Fix migration to create `internships` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- database/migrations/2020_04_17_212802_create_internships_table.php
+++ database/migrations/2020_04_17_212802_create_internships_table.php
@@ -15,13 +15,18 @@ class CreateInternshipsTable extends Migration
     {
         Schema::create('internships', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('offer_id');
             $table->foreign('offer_id')->references('id')->on('offers');
+            $table->foreignId('student_id');
             $table->foreign('student_id')->references('id')->on('students');
+            $table->foreignId('company_supervisor_id');
             $table->foreign('company_supervisor_id')->references('id')->on('users');
+            $table->foreignId('university_supervisor_id');
             $table->foreign('university_supervisor_id')->references('id')->on('users');
             $table->integer('grade');
             $table->dateTime('interview_date');
-            $table->foreign('internships_status_id')->references('id')->on('internship_statuses');
+            $table->foreignId('internship_status_id');
+            $table->foreign('internship_status_id')->references('id')->on('internship_statuses');
             $table->dateTime('created_at', 0);
             $table->dateTime('updated_at', 0);
         });
