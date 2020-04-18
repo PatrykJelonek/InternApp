7e1b7765f07d20053d6e035a03c151b23ca56ae4
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 16:26:24 +0200

    Fix migration to create `user_universities` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- database/migrations/2020_04_17_190114_create_users_universities_table.php
+++ database/migrations/2020_04_17_190114_create_users_universities_table.php
@@ -15,7 +15,9 @@ class CreateUsersUniversitiesTable extends Migration
     {
         Schema::create('users_universities', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('university_id');
             $table->foreign('university_id')->references('id')->on('universities');
             $table->dateTime('created_at', 0);
         });
