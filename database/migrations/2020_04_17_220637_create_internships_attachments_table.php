6325c6e135e83ebfa2419b5b3e9e2b43edc22956
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 16:38:45 +0200

    Fix migration to create `internships_attachments` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- database/migrations/2020_04_17_220637_create_internships_attachments_table.php
+++ database/migrations/2020_04_17_220637_create_internships_attachments_table.php
@@ -15,7 +15,9 @@ class CreateInternshipsAttachmentsTable extends Migration
     {
         Schema::create('internships_attachments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('internship_id');
             $table->foreign('internship_id')->references('id')->on('internships');
+            $table->foreignId('attachment_id');
             $table->foreign('attachment_id')->references('id')->on('attachments');
         });
     }
