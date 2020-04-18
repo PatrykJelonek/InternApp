cd8a5400604341fdd2531057a91f6f41d9eaf538
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 16:20:34 +0200

    Fix migration to create `messages` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- database/migrations/2020_04_17_171324_create_messages_table.php
+++ database/migrations/2020_04_17_171324_create_messages_table.php
@@ -16,7 +16,9 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->string('content', 255);
+            $table->unsignedBigInteger('from_user_id');
             $table->foreign('from_user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('to_user_id');
             $table->foreign('to_user_id')->references('id')->on('users');
             $table->dateTime('created_at', 0);
         });
