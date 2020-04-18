6f60285d3eaa3be153693ad9cf79ec6929019700
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-18 16:29:18 +0200

    Fix migration to create `questionnaires_roles` table
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- database/migrations/2020_04_17_202524_create_questionnaires_roles_table.php
+++ database/migrations/2020_04_17_202524_create_questionnaires_roles_table.php
@@ -15,7 +15,9 @@ class CreateQuestionnairesRolesTable extends Migration
     {
         Schema::create('questionnaires_roles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreignId('questionnaire_id');
             $table->foreign('questionnaire_id')->references('id')->on('questionnaires');
             $table->dateTime('created_at', 0);
         });
