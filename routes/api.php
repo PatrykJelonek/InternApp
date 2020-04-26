8e8c2b6eae8cbceaef4cc8eb42ea5b4551870b4a
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-26 23:13:35 +0200

    Created Api directory for Api Controllers Made `UserController`
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/user_statuses', 'UserStatusController');
+Route::resource('/user_statuses', 'Api\UserStatusController');
