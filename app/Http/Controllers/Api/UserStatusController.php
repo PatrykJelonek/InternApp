c9420af3241b98864258c8a40c662b63d0d154ce
PatrykJelonek <ghostzoombi@gmail.com>
2020-04-27 01:49:52 +0200

    Created update method in UserStatusController.php
    Signed-off-by: Patryk Jelonek <patrykjelonek97@gmail.com>

--- app/Http/Controllers/Api/UserStatusController.php
+++ app/Http/Controllers/Api/UserStatusController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use App\UserStatus;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -12,7 +13,7 @@ class UserStatusController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -27,7 +28,7 @@ class UserStatusController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -37,8 +38,8 @@ class UserStatusController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -57,8 +58,8 @@ class UserStatusController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
@@ -68,8 +69,8 @@ class UserStatusController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function edit($id)
     {
@@ -79,20 +80,30 @@ class UserStatusController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $userStatus = User::find($request->input("id"));
+
+        if (isset($userStatus)) {
+            $userStatus->name = $request->input("name");
+            $userStatus->description = $request->input("description");
+            $userStatus->updated_at = date('Y-m-d H:i:s');
+
+            if ($userStatus->save())
+                return response($userStatus, Response::HTTP_OK);
+        }
+
+        return response("User status has not updated!", Response::HTTP_NOT_MODIFIED);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function destroy($id)
     {
