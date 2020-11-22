5fbd453f7ea8d0b70e801a7e0688b96ae1a9214b
Patryk Jelonek <patrykjelonek97@gmail.com>
2020-11-22 18:57:08 +0100

    Added welcome dashboard component

---
+++ resources/js/views/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid >
+        <h2>Welcome to the dashboard</h2>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Welcome"
+    }
+</script>
+
+<style scoped>
+
+</style>
