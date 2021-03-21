d5f98060e05d9f6ee3cebf3903db9e50ddd1af23
Patryk Jelonek <patrykjelonek97@gmail.com>
2021-03-21 11:42:31 +0100

    Changes

--- resources/js/components/Internship/TheInternshipStudents.vue
+++ resources/js/components/Internship/TheInternshipStudents.vue
@@ -1,60 +1,12 @@
 <template>
-    <div>
-        <p class="text-h5 font-weight-medium">Lista studentów</p>
-        <p class="subtitle-1">Wybierz jednego z poniższych studentów by zobaczyć listę jego wpisów i zadań.</p>
+    <v-card elevation="0">
+        <v-card-title>Lista studentów</v-card-title>
+        <v-card-subtitle>Wybierz jednego ze studentów z poniższej listy by zobaczyć jego wpisy i zadania w dzienniku praktyk.</v-card-subtitle>
         <v-divider></v-divider>
-        <v-row class="mt-5">
-            <v-col cols="3">
-                <v-card>
-                    <v-list dense nav>
-                        <v-list-item to="/universities">
-                            <v-list-item-content>
-                                <v-list-item-title>Jan Nowak</v-list-item-title>
-                                <v-list-item-subtitle>18234</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <v-card>
-                    <v-list>
-                        <v-list-item to="/universities">
-                            <v-list-item-content>
-                                <v-list-item-title>Jan Nowak</v-list-item-title>
-                                <v-list-item-subtitle>18234</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <v-card>
-                    <v-list>
-                        <v-list-item to="/universities">
-                            <v-list-item-content>
-                                <v-list-item-title>Jan Nowak</v-list-item-title>
-                                <v-list-item-subtitle>18234</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-card>
-            </v-col>
-            <v-col cols="3">
-                <v-card>
-                    <v-list>
-                        <v-list-item to="/universities">
-                            <v-list-item-content>
-                                <v-list-item-title>Jan Nowak</v-list-item-title>
-                                <v-list-item-subtitle>18234</v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list>
-                </v-card>
-            </v-col>
-        </v-row>
-    </div>
+        <v-list nav>
 
+        </v-list>
+    </v-card>
 </template>
 
 <script>
