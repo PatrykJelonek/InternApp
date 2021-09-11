ce907bea48572883e203309976b97c559ce41475
patryk-jelonek <patryk.jelonek@furgonetka.pl>
2021-09-11 20:45:28 +0200

    Changes

--- app/Constants/InternshipStatusConstants.php
+++ app/Constants/InternshipStatusConstants.php
@@ -16,6 +16,13 @@ class InternshipStatusConstants
     public const STATUS_ENDED_BY_COMPANY = 'ended_by_company';
     public const STATUS_ENDED_BY_UNIVERSITY = 'ended_by_university';
 
+    public const STATUS_GROUP_NEW = 'new';
+    public const STATUS_GROUP_ACCEPTED = 'accepted';
+    public const STATUS_GROUP_IN_PROGRESS = 'in_progress';
+    public const STATUS_GROUP_ENDED = 'ended';
+    public const STATUS_GROUP_ENDED_BY_COMPANY = 'ended_by_company';
+    public const STATUS_GROUP_ENDED_BY_UNIVERSITY = 'ended_by_university';
+
     public const STATUSES = [
         self::STATUS_NEW,
         self::STATUS_ACCEPTED,
@@ -32,5 +39,14 @@ class InternshipStatusConstants
         self::STATUS_ENDED => 'Zakończony',
         self::STATUS_ENDED_BY_COMPANY => 'Zakończony przez firmę',
         self::STATUS_ENDED_BY_UNIVERSITY => 'Zakończony przez uczelnie',
+    ];
+
+    public const STATUSES_GROUP = [
+        self::STATUS_NEW => self::STATUS_GROUP_NEW,
+        self::STATUS_ACCEPTED => self::STATUS_GROUP_ACCEPTED,
+        self::IN_PROGRESS => self::STATUS_GROUP_IN_PROGRESS,
+        self::STATUS_ENDED => self::STATUS_GROUP_ENDED,
+        self::STATUS_ENDED_BY_COMPANY => self::STATUS_GROUP_ENDED_BY_COMPANY,
+        self::STATUS_ENDED_BY_UNIVERSITY => self::STATUS_GROUP_ENDED_BY_UNIVERSITY,
     ];
 }
