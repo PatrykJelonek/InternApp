6e8d4f30e11c339dcf3b08838220eca74f410f19
patryk-jelonek <patryk.jelonek@furgonetka.pl>
2021-09-14 14:57:32 +0200

    Company Verification

--- resources/js/views/NotFound.vue
+++ resources/js/views/NotFound.vue
@@ -4,6 +4,7 @@
             <h1 class="text-h1 font-weight-bold primary--text">Ups...</h1>
             <h2 class="text-h5 mt-5">Nie udało znaleźć się strony której szukasz.</h2>
             <p class="body-1 mt-5">Status: <code>404</code></p>
+            <p>{{ $route.path }}</p>
             <v-btn
                 outlined
                 color="primary"
@@ -11,6 +12,14 @@
                 @click="$router.go(-2)"
             >
                 Cofnij
+            </v-btn>
+            <v-btn
+                outlined
+                color="primary"
+                class="mt-5"
+                @click="$router.push({name: 'landing-page'})"
+            >
+                Strona Główna
             </v-btn>
         </v-container>
     </v-app>
